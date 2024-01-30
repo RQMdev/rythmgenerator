996343918ab825d7645f3e7295789b885ebd526c
Benoit 'RQM-' Laurent <blaurent@makemusic.com>
2024-01-30 15:11:29 +0100

    add build for github pages

---
+++ build/precache-manifest.eb5c5fb32cd64f58bbdce42a17e8df56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c3d12f3e3ccbae6bd5441f77538930",
+    "url": "/Musisync-KVLZ.ttf"
+  },
+  {
+    "revision": "7663fd6b156c6882e4b66bd72556e0ee",
+    "url": "/Musisync-qYy6.ttf"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "21929a0ea107b5bd01a78b02583dd979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "bd247a1ca08bd7fb2dc2",
+    "url": "/static/css/main.7c4b2ea8.chunk.css"
+  },
+  {
+    "revision": "bd247a1ca08bd7fb2dc2",
+    "url": "/static/js/main.bd247a1c.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "/static/js/vendors~main.ded87515.chunk.js"
+  },
+  {
+    "revision": "21c3d12f3e3ccbae6bd5441f77538930",
+    "url": "/static/media/Musisync-KVLZ.3031568e.ttf"
+  }
+]);
